f34e178aa41b8646fb0b476e9a80e6fa32a02d80
rainbowzom <rainbowzom@gmail.com>
2021-04-22 22:08:01 -0700

    Bot now works and messages top builds items if they exist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import os
 import config
+import requests
+from bs4 import BeautifulSoup
 
 client = discord.Client()
 
@@ -20,11 +22,39 @@ async def on_message(message):
         if len(words) <2:
             await message.channel.send("Please enter a champion name!")
             return
-        if len(words) < 3:
-            await message.channel.send("Showing the top pro build for " + words[1].capitalize())
+        if len(words) >= 2:
+            champ = ""
+            for i in range(1, len(words)):
+                champ+=words[i]
+            page = requests.get("https://probuildstats.com/champion/" + champ)
+            soup= BeautifulSoup(page.content, "lxml")
+            result = soup.find_all("div", class_="top-players")
+            if len(result) == 0:
+                await message.channel.send("Could not find top pro builds for this champion. \nPlease refer to  "+ "https://probuildstats.com/champion/"+ words[1])
+                return
+            summaries = result[0].find_all("div", class_ = "match-summary")
+            result = "Builds:\n\n"
+            for summary in summaries:
+                player = summary.find_all("a",class_="name")[0]
+                team = summary.find_all("div",class_="team-name")[0]
+             
+                result += "Player: " + player.text + ",  Team: " + team.text + "\n"
+
+                
+                items = summary.find_all("div", class_ = "items")
+                imgs = items[0].find_all("img", alt = True)
+                result += "Items: "
+                for img in imgs:
+                    result +=img['alt'] + ", "
+                    print(img['alt'])
+                result += "\n"
+                result +="\n"
+
+            
+            
+            await message.channel.send("Showing the top pro builds for " + champ.capitalize() +"\n" + result)
+          
             return
-        if len(words) < 4:
-            await message.channel.send("Showing the top pro build for " + words[1].capitalize() + " in lane: " + words[2].capitalize())
         
 
 client.run(config.dc_token)
